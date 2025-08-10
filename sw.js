dbb7d6df55cbb72a2bb9202c44abf721c42d5ade
TheAtlanticShredder <adam.jewell@maine.edu>
2025-08-10 03:33:28 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,32 +1,24 @@
-const CACHE = "neon-dodge-v1";
+const CACHE = "cypher-math-v1";
 const ASSETS = [
-  "./neon-dodge.html",
+  "./",
+  "./index.html",
   "./manifest.webmanifest",
   "./sw.js",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
 ];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+self.addEventListener("install", (e)=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
   self.skipWaiting();
 });
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => k === CACHE ? null : caches.delete(k))))
+self.addEventListener("activate", (e)=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.map(k=>k===CACHE?null:caches.delete(k))))
   );
   self.clients.claim();
 });
-
-self.addEventListener("fetch", (event) => {
-  const req = event.request;
-  // Try cache, then network
-  event.respondWith(
-    caches.match(req).then((cached) => {
-      return cached || fetch(req).then((res) => {
-        return res;
-      });
-    })
+self.addEventListener("fetch", (e)=>{
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
